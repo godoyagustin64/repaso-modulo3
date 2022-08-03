f8a2cbb81883622729aaaa6b32a3ffeab1458e68
Agustin Nahuel Godoy Gaetan <AGodoy@veng.local>
2022-08-03 09:53:14 -0300

    Añadiendo carpeta public

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require ("express");
+const path = require ("path");
 const app = express();
+
+// Registrar como publica la carpeta public
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../publica")));
+
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a la pagina principal");
-})
+});
+
+
+
+
 
 app.listen(3000, () => console.log("Proyecto en el puerto 3000"));
 
