929bbe96a60577f9247df2c49253386ffc9c6700
Agustin Nahuel Godoy Gaetan <AGodoy@veng.local>
2022-08-03 10:54:03 -0300

    añadiendo ejs

--- src/app.js
+++ src/app.js
@@ -5,17 +5,27 @@ const app = express();
 // Registrar como publica la carpeta public
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use("/prefijo", express.static(path.join(__dirname, "../publica")));
 
 // Registrar carpetas estaticas con "prefijos"
 app.use("/desktop", express.static("desktop"));
 app.use("/mobile", express.static("mobile"));
+
+// Registrar ejs como motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a la pagina principal");
 });
 
 
+app.get("/index", (req, res) => {
+    res.render("index");
+});
+
+app.get("/index2", (req, res) => {
+    res.render("index2");
+});
 
 
 
